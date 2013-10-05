3f846c69b21cee243535e85934ea5b07128c128d
Ryan Salomon <ryan5231@gmail.com>
2013-10-04 22:17:01 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LaurenBlog::Application.config.secret_key_base = 'd6cff567c6331a270d3c72cc2015570dae0d5ab881497af85109b1871427e293ca34fe38ab8759ceebd41c0bc093b9edbb50b34bda175524553625b5cd13beec'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+LaurenBlog::Application.config.session_store :cookie_store, key: '_lauren_blog_session'
